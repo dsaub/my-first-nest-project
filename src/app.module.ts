8e2b4b51090669d5837a8689a1bd467a8f4e8816
Daniel <danilacasito8@gmail.com>
2025-11-17 09:55:50 +0100

    Create Auth data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DepartmentsModule } from './departments/departments.module';
 import { DepartmentsService } from './departments/departments.service';
 import { PrismaService } from './prisma.service';
+import { EmployeesModule } from './employees/employees.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DepartmentsModule],
+  imports: [DepartmentsModule, EmployeesModule, AuthModule],
   controllers: [],
   providers: [DepartmentsService, PrismaService],
 })
